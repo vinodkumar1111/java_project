b9637b4fcaca2d1b4c7c930b8f9b849c3cad9992
vinod <ramavathvinodkumar145@gmailcom>
2025-11-26 12:24:08 +0530

    webhooks

--- src/test/java/com/example/App.java
+++ src/test/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 
 public class App {
     public String getMessage() {
-        return "Hello Jenkins, Testing webhooks!";
+        return "Hello Jenkins, Testing webhooks..";
     }
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
